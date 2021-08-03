350fce6ee4c718a80f31313d3985bc31fdaaf378
scammi <scammi@gmail.com>
2021-08-03 17:08:44 -0300

    Initial Commit

---
+++ cravovia-client/cravovia_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  cravovia_clientApp.swift
+//  cravovia-client
+//
+//  Created by SantiCammi on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct cravovia_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
