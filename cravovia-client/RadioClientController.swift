f63e0b48044920e4b67775071c4f21e266919624
scammi <scammi@gmail.com>
2021-08-13 18:56:40 -0300

    Get song metadata

--- cravovia-client/RadioClientController.swift
+++ cravovia-client/RadioClientController.swift
@@ -12,7 +12,8 @@ import MediaPlayer
 var player: AVPlayer?
 var playing = false
 
-struct RadioClient {
+class RadioClient: NSObject, AVPlayerItemMetadataOutputPushDelegate {
+    weak var myDelegate: AVPlayerItemMetadataOutputPushDelegate?
     
     func play() {
         if !playing
@@ -20,7 +21,12 @@ struct RadioClient {
             NSLog("playing")
             
             let urlCracovia = URL(string: "https://tolkien.republicahosting.net:1614/live")
-            let playerItem:AVPlayerItem = AVPlayerItem(url: urlCracovia!)
+            let playerItem: AVPlayerItem = AVPlayerItem(url: urlCracovia!)
+            
+            let metadataOutput = AVPlayerItemMetadataOutput(identifiers: nil)
+            metadataOutput.setDelegate(self, queue: DispatchQueue.main)
+            playerItem.add(metadataOutput)
+            
             player = AVPlayer(playerItem: playerItem)
             playing = true
             
@@ -33,7 +39,6 @@ struct RadioClient {
             nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = "album title"
             
             MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
-            
         }
         player?.play()
     }
@@ -42,5 +47,16 @@ struct RadioClient {
         NSLog("pause")
         player?.pause()
         playing = false
+    }
+    
+    func metadataOutput(_ output: AVPlayerItemMetadataOutput, didOutputTimedMetadataGroups groups: [AVTimedMetadataGroup], from track: AVPlayerItemTrack?) {
+        if let item = groups.first?.items.first // make this an AVMetadata item
+        {
+            item.value(forKeyPath: "value") // looking for that key bro
+            let Song = (item.value(forKeyPath: "value")!)
+             NSLog("Now Playing: \n \(Song)") // print the results
+        } else {
+            NSLog("MetaData Error") // No Metadata or Could not read
+        }
     }
 }
